3dcde4bc13dac2892aa4dc7e81efc0c26bcbcf98
Lucas Castilho <lucas.hco@hotmail.com>
2022-09-10 19:42:23 -0300

    treinamento http

---
+++ index.js
@@ -0,0 +1,42 @@
+const api = require('./api')
+
+
+// user data
+// id: string 
+// name: string
+// age: string
+
+
+async function addUser() {
+  // const newUser = {
+  //   id: new Date().getTime().toString(),
+  //   name: 'Jhon Doe',
+  //   age: 99
+  // }
+
+  // const result = await api.post('/users', newUser)
+
+  // console.log({result})
+}
+
+function getUser(userId) {
+
+}
+
+function getUsers() {
+
+}
+
+function updateUser(userId) {
+
+}
+
+function deleteUser(userId) {
+
+}
+
+addUser()
+// getUser(userId)
+// getUsers()
+// updateUser(userId)
+// deleteUser(userId)
